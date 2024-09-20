2593317ea85160ff3d80df11eaee828b93eed54b
Michael Gruper <sarge43210@gmail.com>
2024-09-20 08:04:58 -0500

    initial commit

--- GradesFunctionAppGruper/ViewController.swift
+++ GradesFunctionAppGruper/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pointsEarnedTextField: UITextField!
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var pointsPossibleTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButtonAction(_ sender: UIButton) {
+        if passed(i: Double(pointsEarnedTextField.text!) ?? 0, j: Double(pointsPossibleTextField.text!) ?? 0){
+            labelOutlet.text = "You Passed!"
+        }
+        else{
+            labelOutlet.text = "You Failed!"
+        }
+    }
+    func passed(i: Double, j: Double) -> Bool {
+        if i/j <= 0.59{
+            return false
+        }
+        else{
+            return true
+        }
+    }
 }
 
