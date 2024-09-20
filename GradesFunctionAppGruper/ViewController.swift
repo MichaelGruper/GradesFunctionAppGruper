b22c9aef7698a28f2cc71b5a5e86a97bfa1ae6ad
Michael Gruper <sarge43210@gmail.com>
2024-09-20 07:46:48 -0500

    Initial Commit

---
+++ GradesFunctionAppGruper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradesFunctionAppGruper
+//
+//  Created by MICHAEL GRUPER on 9/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GradesFunctionAppGruperTests/GradesFunctionAppGruperTests.swift
@@ -0,0 +1,17 @@
+//
+//  GradesFunctionAppGruperTests.swift
+//  GradesFunctionAppGruperTests
+//
+//  Created by MICHAEL GRUPER on 9/20/24.
+//
+
+import Testing
+@testable import GradesFunctionAppGruper
+
+struct GradesFunctionAppGruperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
